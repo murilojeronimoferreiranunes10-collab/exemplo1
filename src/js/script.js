5f76512b270f2df950575f7d0cd7706102d2b840
murilo <murilojeronimoferreiranunes10@gmail.com>
2025-09-29 11:32:23 -0300

    Declarando variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-alert("hello word")
+//alert("hello word")
+// DECLARAÇÃO E VARIAVEIS
+
+
+var nome = "FIAP";
+console.log(typeof nome)
+
+
+let nome1 = 10;
+console.log(typeof nome1)
+
+const nome2 = {}
+console.log(typeof nome2)
+
+let nome3 = [];
+console.log(typeof nome3)
+
+let nome4=true;
+console.log(typeof nome4)
+
+//{} objeto / lista - objeto(atributos ao objeto e seus valores)
+//[] array / lista - array (recebe um monte de objeto)
+//() parametro / metodos 
+
